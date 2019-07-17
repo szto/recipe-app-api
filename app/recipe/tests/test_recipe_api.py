7f5c9b869bce581a076a545bd511eba828cf4ea3
David Kim <obadiah26@gmail.com>
2019-07-18 02:27:40 +0900

    Added working list tags endpoint.

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,78 @@
+# from django.contrib.auth import get_user_model
+# from django.test import TestCase
+# from django.urls import reverse
+
+# from rest_framework import status
+# from rest_framework.test import APIClient
+
+# from core.models import Recipe
+
+# from recipe.serializers import RecipeSerializer
+
+
+# RECIPES_URL = reverse('recipe:recipe-list')
+
+
+# def sample_recipe(user, **params):
+#     """Create and return a sample recipe"""
+#     defaults = {
+#         'title': 'Sample recile',
+#         'time_mionutes': 10,
+#         'price': 5.00
+#     }
+#     defaults.update(params)
+
+#     return Recipe.objects.create(user=user, **defaults)
+
+
+# class PublicRecipeApiTests(TestCase):
+#     """Test unauthenticated recipe API access"""
+
+#     def setUp(self):
+#         self.client = APIClient()
+
+#     def test_auth_required(self):
+#         res = self.client.get(RECIPES_UR)
+
+#         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHROIZED)
+
+
+# class PrivateRecipeApiTestes(TestCase):
+#     """Test unauthenticated recipe API access"""
+
+#     def setUp(self):
+#         self.client = APIClient()
+#         self.user = get_user_model().obejcts.create_user(
+#             'test@londonappendve.com',
+#             'testpass'
+#         )
+#         self.client.force_authenticate(self.user)
+
+#     def test_retrieve_recipes(self):
+#         """Test retrieveing a list of recipes"""
+#         sample_recipe(userr=self.user)
+#         sample_recipe(userr=self.user)
+
+#         res = self.client.get(RECIPES_URL)
+
+#         recipes = Recipe.obejcts.all().order_by('-id')
+#         serializer = RecipeSerializer(recipes, many=True)
+#         self.assertEqual(res,status_code, status.HTTP_200_OK)
+#         self.assertEqual(res.data, serializer.data)
+
+#     def test_recipes_limited_to_user(self):
+#         "Test retrieving recipes for user"
+#         user2 = get_user_model().objects.create_user(
+#             'other@london.com',
+#             'password123'
+#         )
+#         sample_recipe(user=user2)
+#         sample_recipe(user=self.user)
+
+#         res = self.client.get(RECIPES_URL)
+
+#         recipes = Recipe.objects.filter(user=self.user)
+#         serializer = RecipeSerializer(recipes, manty=True)
+#         self.assertEqual(res.status_code, status.HTTP_200_OK)
+#         self.assertEqual(len(rs.data), 1)
+#         self.assertEqual(res.data, serializer.data)
