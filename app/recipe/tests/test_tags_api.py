abcb2798f3f63ce709660066d5bbb55e6bf79b0a
David Kim <obadiah26@gmail.com>
2019-07-20 16:28:41 +0900

    Added recipe image feature.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -78,7 +78,6 @@ class PrivateTagsApiTests(TestCase):
     def test_create_tag_invalid(self):
         """Test creating a new tag with invalid payload"""
         payload = {'name': ''}
-        res =self.client.post(TAGS_URL, payload)
+        res = self.client.post(TAGS_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
